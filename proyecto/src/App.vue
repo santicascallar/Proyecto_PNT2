a1c5936beceb54ff2965bc1719e04a0df0e6bd7f
Lautaro <laulucianiconde@gmail.com>
2025-09-16 22:02:32 -0300

    Primer commit de Lauti

--- proyecto/src/App.vue
+++ proyecto/src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h1>HOLA MUNDO SOY UN CAPO</h1>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
