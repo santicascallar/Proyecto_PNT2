18c1f5f5cc69e12de35e4e017d5330222a611a19
mjdalesio <dalesiomatias@gmail.com>
2025-09-16 21:59:54 -0300

    Prueba primer commit

--- proyecto/src/App.vue
+++ proyecto/src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h2>hola mundo, estoy a prueba</h2>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
