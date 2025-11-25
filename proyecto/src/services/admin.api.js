b28c8e35d421df3506fac22b743599e30a3c1225
Santi Cascallar <Santi Cascallar@SANTI>
2025-11-25 18:42:23 -0300

    fix

--- proyecto/src/services/admin.api.js
+++
@@ -1,9 +0,0 @@
-export async function fetchLeastReservationsReport() {
-  throw new Error("Reports API removed");
-}
-
-export async function fetchTopRestaurantsReport() {
-  throw new Error("Reports API removed");
-}
-
-export default { fetchLeastReservationsReport, fetchTopRestaurantsReport };
